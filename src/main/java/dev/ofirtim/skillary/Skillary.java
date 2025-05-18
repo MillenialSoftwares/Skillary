ec8a800f0d7bbf1c1ce6b852c8d25144258b737d
ofirtim <ofirrr32@gmail.com>
2025-05-18 14:44:42 +0300

    Initial commit

---
+++ src/main/java/dev/ofirtim/skillary/Skillary.java
@@ -0,0 +1,17 @@
+package dev.ofirtim.skillary;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Skillary extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
